9c549d27ae519a2f33f3bedd90d25e15674fde5f
arizkyrahman <rizkyrahman7202@gmail.com>
2023-06-03 16:06:08 +0700

    initial starter

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame, sys
+from settings import *
+
+# from editor import Editor
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        # self.editor = Editor()
+    
+    def run(self):
+        while True:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit
+
+            # self.editor.run(dt)
+            pygame.display.update()
+    
+if __name__ == '__main__':
+    main = Main()
+    main.run()
